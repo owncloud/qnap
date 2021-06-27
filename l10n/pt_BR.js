9c814bdd0269e75a1b3e06e6033a4d0a4b6de2fa
Michael Barz <mbarz@owncloud.com>
2021-06-27 00:07:38 +0000

    [tx] updated from transifex

---
+++ l10n/pt_BR.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "qnap",
+    {
+    "QNAP" : "QNAP",
+    "Action Required: Your ownCloud licenses' user limit was exceeded" : "Ação necessária: O limite de usuários de suas próprias licenças do ownCloud foi excedido",
+    "ownCloud on QNAP" : "ownCloud no QNAP",
+    "The maximum user limit was exceeded. " : "O limite máximo de usuários foi excedido.",
+    "To add more users, purchase a license from https://software.qnap.com/owncloud.html" : "Para adicionar mais usuários, adquira uma licença em https://software.qnap.com/owncloud.html",
+    "Dear ownCloud - Admin," : "Caro ownCloud - Admin,",
+    "The maximum user limit was exceeded." : "O limite máximo de usuários foi excedido.",
+    "Cheers!" : "Saudações!",
+    "ownCloud License on QNAP" : "Licença ownCloud no QNAP",
+    "Active users: %d" : "Usuários ativos: %d",
+    "Licensed users: %d" : "Usuários licenciados:%d",
+    "Visit the QNAP Store to purchase more licenses." : "Visite a Loja QNAP para adquirir mais licenças.",
+    "License Overview" : "Visão Geral da Licença",
+    "License ID" : "ID de licença",
+    "Valid from" : "Válido de",
+    "Valid until" : "Válido até",
+    "Number of users" : "Número de usuários"
+},
+"nplurals=2; plural=(n > 1);");
