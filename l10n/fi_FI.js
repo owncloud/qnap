d5dcb7c1610e9b1c446a41e0265593caead1dd06
Michael Barz <mbarz@owncloud.com>
2021-07-22 01:07:20 +0000

    [tx] updated from transifex

---
+++ l10n/fi_FI.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "qnap",
+    {
+    "QNAP" : "QNAP",
+    "Action required: Your ownCloud user limit is exceeded" : "Toimia vaaditaan: ownCloud käyttäjäraja on ylitetty",
+    "User limit exceeded. To add more users click here." : "Käyttäjämäärä ylitetty. Lisää käyttäjiä klikkaamalla tästä.",
+    "Dear ownCloud admin," : "Arvoisa ownCloud-pääkäyttäjä,",
+    "Your user limit is exceeded." : "Käyttäjäraja on ylitetty.",
+    "To add or enable additional users, please see your licensing options at https://software.qnap.com/owncloud.html." : "Ottaaksesi käyttöön tai lisätäksesi käyttäjiä, tutustu lisenssivaihtoehtoihin osoitteessa https://software.qnap.com/owncloud.html.",
+    "Cheers!" : "Kiitos!",
+    "User and License Information" : "Käyttäjä- ja lisenssitiedot",
+    "ownCloud for QNAP comes as Community Edition by default and is free to use for up to 5 users." : "ownCloud for QNAP on oletusarvoisest Community Edition ja on maksuton 5 käyttäjään asti.",
+    "If you need more than 5 users or want to use any of our <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">Enterprise Apps from the Marketplace</a>, you need to upgrade to Enterprise Edition by purchasing licenses from the <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP Software Store</a>." : "Jos tarvitset yli 5 käyttäjää, tai haluat käyttää <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">Enterprise Apps -sovelluksia</a>, sinun on hankittava Enterprise Edition -lisenssi <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP-sovelluskaupasta</a>.",
+    "Guest users don't count as normal users, that means you can invite as many as needed." : "Vieraskäyttäjiä ei lasketa normaalikäyttäjiksi, joten voit kutsua niin monta vieraskäyttäjää kuin haluat.",
+    "Usage: %d of %d licensed users" : "Käyttö: %d / %d lisensoitua käyttäjää",
+    "License Overview" : "Lisenssin yleiskatsaus",
+    "License ID" : "Lisenssinumero",
+    "Valid from" : "Voimassa alkaen",
+    "Valid until" : "Kelvollinen",
+    "Number of users" : "Käyttäjämäärä",
+    "Status" : "Tila"
+},
+"nplurals=2; plural=(n != 1);");
