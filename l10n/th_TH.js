cff9654d7487ab826267ff7dda779fae2cf0de24
Michael Barz <mbarz@owncloud.com>
2021-08-28 00:09:43 +0000

    [tx] updated from transifex

---
+++ l10n/th_TH.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "qnap",
+    {
+    "QNAP" : "QNAP",
+    "Action required: Your ownCloud user limit is exceeded" : "หมายเหตุ: เกินขีดจำกัดผู้ใช้ ownCloud ของคุณ",
+    "User limit exceeded. To add more users click here." : "ผู้ใช้เกินขีดจำกัด หากต้องการเพิ่มผู้ใช้เพิ่มเติมคลิกที่นี่",
+    "Dear ownCloud admin," : "เรียน ผู้ดูแลระบบ ownCloud",
+    "Your user limit is exceeded." : "ผู้ใช้ของคุณเกินขีดจำกัด",
+    "Cheers!" : "ไชโย!",
+    "User and License Information" : "ข้อมูลผู้ใช้และใบอนุญาต",
+    "ownCloud for QNAP comes as Community Edition by default and is free to use for up to 5 users." : "ownCloud สำหรับ QNAP มาในรูปแบบ Community Edition โดยค่าเริ่มต้นและใช้งานได้ฟรีสำหรับผู้ใช้สูงสุด 5 คน",
+    "If you need more than 5 users or want to use any of our <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">Enterprise Apps from the Marketplace</a>, you need to upgrade to Enterprise Edition by purchasing licenses from the <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP Software Store</a>." : "หากคุณต้องการผู้ใช้มากกว่า 5 รายหรือต้องการใช้ <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">Enterprise Apps ของเราจาก Marketplace</a> คุณต้องอัปเกรดเป็น Enterprise Edition โดยการซื้อใบอนุญาตจาก <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP Software Store</a>",
+    "Guest users don't count as normal users, that means you can invite as many as needed." : "ผู้ใช้ทั่วไปไม่นับเป็นผู้ใช้ทั่วไป ซึ่งหมายความว่าคุณสามารถเชิญได้มากเท่าที่ต้องการ",
+    "Usage: %d of %d licensed users" : "การใช้งาน: %d จาก %d ของผู้ใช้ที่ได้รับใบอนุญาต",
+    "License Overview" : "ภาพรวมใบอนุญาต",
+    "License ID" : "รหัสใบอนุญาต",
+    "Valid from" : "ใช้ได้ตั้งแต่",
+    "Valid until" : "ใช้ได้จนถึง",
+    "Number of users" : "จำนวนผู้ใช้",
+    "Status" : "สถานะ"
+},
+"nplurals=1; plural=0;");
