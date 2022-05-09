5c30c05df13ced20b2c066076fbf867fd6b9fd54
ownClouders <devops@owncloud.com>
2022-05-09 00:05:55 +0000

    [tx] updated from transifex

---
+++ l10n/es.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "qnap",
+    {
+    "QNAP" : "QNAP",
+    "Action required: Your ownCloud user limit is exceeded" : "Acción requerida: Se excedió el límite de usuarios de ownCloud",
+    "User limit exceeded. To add more users click here." : "Límite de usuarios excedido. Para agregar más usuarios haga clic aquí.",
+    "Dear ownCloud admin," : "Estimado administrador ownCloud,",
+    "Your user limit is exceeded." : "Tu límite de usuarios está excedido.",
+    "To add or enable additional users, please see https://software.qnap.com/owncloud.html for your licensing options." : "Para agregar o habilitar usuarios adicionales, por favor vea en https://software.qnap.com/owncloud.html sus opciones de licencia.",
+    "Cheers!" : "¡Saludos!",
+    "User and License Information" : "Información de usuario y licencia",
+    "ownCloud for QNAP comes as Community Edition by default and is free to use for up to 5 users." : "ownCloud para QNAP viene como Community Edition de forma predeterminada y es de uso gratuito para hasta 5 usuarios.",
+    "If you need more than 5 users or want to use any of our <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">Enterprise Apps from the Marketplace</a>, you need to upgrade to Enterprise Edition by purchasing licenses from the <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP Software Store</a>." : "Si necesita más de 5 usuarios o desea utilizar cualquiera de nuestras <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">Enterprise Apps desde el Marketplace</a>, necesita actualizar su Enterprise Edition adquiriendo licencias desde el <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP Software Store </a>.",
+    "Guest users don't count as normal users, that means you can invite as many as needed." : "Los usuarios invitados no cuentan como usuarios normales, lo que significa que puede invitar a tantos como necesite.",
+    "Usage: %d of %d licensed users" : "Uso: %d de %dusuarios con licencia",
+    "License Overview" : "Vista general de la licencia",
+    "License ID" : "ID de licencia",
+    "Valid from" : "Válido desde",
+    "Valid until" : "Válido hasta",
+    "Number of users" : "Número de usuarios",
+    "Status" : "Estado"
+},
+"nplurals=2; plural=(n != 1);");
