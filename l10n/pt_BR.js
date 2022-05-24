ae22531a8c51e3fa19b5a36237f75f2f80431df6
ownClouders <devops@owncloud.com>
2022-05-24 00:35:51 +0000

    [tx] updated from transifex

--- l10n/pt_BR.js
+++ l10n/pt_BR.js
@@ -20,4 +20,4 @@ OC.L10N.register(
     "Number of users" : "Número de usuários",
     "Status" : "Status"
 },
-"nplurals=2; plural=(n > 1);");
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
