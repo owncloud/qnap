ec767c1b890443a393400653f80531fc5c6276a9
ownClouders <devops@owncloud.com>
2022-10-09 01:28:22 +0000

    [tx] updated from transifex

---
+++ l10n/bg_BG.js
@@ -0,0 +1,23 @@
+OC.L10N.register(
+    "qnap",
+    {
+    "QNAP" : "QNAP",
+    "Action required: Your ownCloud user limit is exceeded" : "Необходимо е действие: Лимитът на потребителите на ownCloud е надхвърлен",
+    "User limit exceeded. To add more users click here." : "Превишаване на лимита на потребителя. За да добавите още потребители, натиснете тук.",
+    "Dear ownCloud admin," : "Уважаеми ownCloud администратор,",
+    "Your user limit is exceeded." : "Лимитът на потребителите ви е надхвърлен.",
+    "To add or enable additional users, please see https://software.qnap.com/owncloud.html for your licensing options." : "За да добавите или активирате допълнителни потребители, моля вижте https://software.qnap.com/owncloud.html за възможностите за лицензиране.",
+    "Cheers!" : "Поздрави!",
+    "User and License Information" : "Информация за потребителя и лиценза",
+    "ownCloud for QNAP comes as Community Edition by default and is free to use for up to 5 users." : "ownCloud за QNAP се предлага като Community Edition по подразбиране и е безплатен за използване от до 5 потребители.",
+    "If you need more than 5 users or want to use any of our <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">Enterprise Apps from the Marketplace</a>, you need to upgrade to Enterprise Edition by purchasing licenses from the <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP Software Store</a>." : "Ако имате нужда от повече от 5 потребители или искате да използвате някое от нашите<a href=\"%s\" target=\"_blank\" rel=\"noreferrer\"> корпоративни приложения от Marketplace</a>, трябва да надстроите до Enterprise Edition, като закупите лицензи от <a href=\"%s\" target=\"_blank\" rel=\"noreferrer\">QNAP Software Store</a>.",
+    "Guest users don't count as normal users, that means you can invite as many as needed." : "Гост-потребителите не се броят за нормални потребители, което означава, че можете да поканите толкова, колкото е необходимо.",
+    "Usage: %d of %d licensed users" : "Използване: %d на %d лицензирани потребители",
+    "License Overview" : "Преглед на лиценза",
+    "License ID" : "Лиценз ID",
+    "Valid from" : "Валидeн от",
+    "Valid until" : "Валиден до",
+    "Number of users" : "Брой потребители",
+    "Status" : "Статус"
+},
+"nplurals=2; plural=(n != 1);");
