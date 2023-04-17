50eb2cba70b2aae2f8125c9e1ceca7904cc7babd
ownClouders <devops@owncloud.com>
2023-04-17 00:34:22 +0000

    [tx] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "qnap",
+    {
+    "QNAP" : "QNAP",
+    "Action required: Your ownCloud user limit is exceeded" : "נדרשת התערבות: הגעת למגבלת המשתמשים שלך ב־ownCloud",
+    "User limit exceeded. To add more users click here." : "הגעת למגבלת המשתמשים. כדי להוסיף עוד משתמשים יש ללחוץ כאן.",
+    "Dear ownCloud admin," : "הנהלת ownCloud יקרה,",
+    "Your user limit is exceeded." : "הגעת למגבלת המשתמשים שלך.",
+    "To add or enable additional users, please see https://software.qnap.com/owncloud.html for your licensing options." : "כדי להוסיף או לאפשר משתמשים נוספים, נא לעיין ב־https://software.qnap.com/owncloud.html לאפשרויות הרישוי שלך.",
+    "Cheers!" : "לחיים!",
+    "User and License Information" : "פרטי משתמשים ורישיון",
+    "ownCloud for QNAP comes as Community Edition by default and is free to use for up to 5 users." : "ownCloud ל־QNAP מושק כמהדורה קהילתית כברירת מחדל והשימוש בו חופשי עד 5 משתמשים.",
+    "Guest users don't count as normal users, that means you can invite as many as needed." : "משתמשים אורחים לא נחשבים למשתמשים רגילים, כלומר שניתן להזמין כמה שצריך.",
+    "Usage: %d of %d licensed users" : "ניצולת: %d מתוך %d משתמשים ברישיון",
+    "License Overview" : "סקירת רישיון",
+    "License ID" : "מזהה רישיון",
+    "Valid from" : "בר תוקף מ-",
+    "Valid until" : "בתוקף עד",
+    "Number of users" : "מספר משתמשים",
+    "Status" : "מצב"
+},
+"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
